a3ee4f0a1360e499e9a47c1d815b1efc60597481
Sarah Kamel <106119803+SarahNabilKamel@users.noreply.github.com>
2023-08-25 04:52:25 +0300

    trying

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,10 +2,9 @@
 #include "lists.h"
 
 /**
-  * print_list - prints all the elements of a linked list
- * @h: pointer to the list_t list to print
+ * main - check the code
  *
- * Return: the number of nodes printed 
+ * Return: Always 0.
  */
 size_t print_list(const list_t *h)
 {
