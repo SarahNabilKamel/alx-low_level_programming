ea81c18e3e512a66790f90d98d13ccd3743cbf55
Sarah Kamel <106119803+SarahNabilKamel@users.noreply.github.com>
2023-09-04 23:45:52 +0300

    update

--- x15-file_io/0-read_textfile.c
+++ x15-file_io/0-read_textfile.c
@@ -10,6 +10,18 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile- Read text file print to STDOUT.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w- actual number of bytes read and printed
+ *        0 when function fails or filename is NULL.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
 	char *buf;
 	ssize_t fd;
 	ssize_t w;
@@ -25,4 +37,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	free(buf);
 	close(fd);
 	return (w);
+}
+
 }
