4841d354a114d6e04f29376621e68e5e7e938f6a
Sarah Kamel <106119803+SarahNabilKamel@users.noreply.github.com>
2023-09-04 23:48:11 +0300

    Update 0-read_textfile.c

--- x15-file_io/0-read_textfile.c
+++ x15-file_io/0-read_textfile.c
@@ -2,41 +2,27 @@
 #include <stdlib.h>
 
 /**
- * read_textfile- Read text file print to STDOUT.
- * @filename: text file being read
- * @letters: number of letters to be read
- * Return: w- actual number of bytes read and printed
- *        0 when function fails or filename is NULL.
+ * read_textfile- reads a text file and
+ * prints it to the POSIX standard output.
+ * @filename: file name
+ * @letters: letters
+ * Return: number of letters or 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	#include "main.h"
-#include <stdlib.h>
-
-/**
- * read_textfile- Read text file print to STDOUT.
- * @filename: text file being read
- * @letters: number of letters to be read
- * Return: w- actual number of bytes read and printed
- *        0 when function fails or filename is NULL.
- */
-ssize_t read_textfile(const char *filename, size_t letters)
-{
-	char *buf;
-	ssize_t fd;
+	char *c;
+	ssize_t x;
 	ssize_t w;
 	ssize_t t;
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	x = open(filename, O_RDONLY);
+	if (x == -1)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	t = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, t);
+	c = malloc(sizeof(char) * letters);
+	t = read(x, c, letters);
+	w = write(STDOUT_FILENO, c, t);
 
-	free(buf);
-	close(fd);
+	free(c);
+	close(x);
 	return (w);
-}
-
 }
